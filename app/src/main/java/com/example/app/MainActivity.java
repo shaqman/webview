2a5a249bb17491ad6bd03592697e7949139cb7e8
Syakur Rahman <bS.xx7_37@yahoo.com>
2018-08-27 13:41:24 +0700

    * Bump android version * Show logo when loading, instead of background color.

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,22 +2,27 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.constraint.ConstraintLayout;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
+    private final static String schema = "http";
+    private final static String domain = "example.com";
+    private final static String url = schema + "://" + domain;
+
     private WebView mWebView;
+    private ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mLayout = findViewById(R.id.logoLayout);
 
         // Force links and redirects to open in the WebView instead of in a browser
         mWebView.setWebViewClient(new WebViewClient());
@@ -27,17 +32,35 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("http://example.com");
-        // mWebView.setWebViewClient(new MyWebViewClient());
+//        mWebView.loadUrl(url);
+//        mWebView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+//                if (Uri.parse(request.getUrl().toString()).getHost().endsWith(domain)) {
+//                    return false;
+//                }
+//
+//                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(request.getUrl().toString()));
+//                view.getContext().startActivity(intent);
+//                return true;
+//            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                mLayout.setVisibility(View.GONE);
+//                mWebView.setVisibility(View.VISIBLE);
+//            }
+//        });
 
         // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+//        mWebView.loadUrl("file:///android_asset/index.html");
     }
 
     // Prevent the back-button from closing the app
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
